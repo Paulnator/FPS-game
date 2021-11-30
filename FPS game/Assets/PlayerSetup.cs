eb86888ea6f8acce74ce735a8ffbf555525c70fe
Paulnator <Paulnatoryt@gmail.com>
2021-11-30 21:12:32 +0100

    CommitV1
    -working basic NetManager system
    -working movement of player
    -working look around of the player

---
+++ FPS game/Assets/PlayerSetup.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour {
+
+    [SerializeField]
+    Behaviour[] componentsToDisable;
+
+    Camera SceneCamera;
+
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            for(int i = 0; i < componentsToDisable.Length; i++)
+            {
+                componentsToDisable[i].enabled = false;
+            }
+        }
+        else
+        {
+            SceneCamera = Camera.main;
+            if(SceneCamera != null)
+            {
+                SceneCamera.gameObject.SetActive(false);
+            }
+            
+        }
+    }
+
+    void OnDisable()
+    {
+        if(SceneCamera != null)
+        {
+            SceneCamera.gameObject.SetActive(true);
+        }
+    }
+
+
+}
